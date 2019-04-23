d828b22f4e88d34930556ae6e61a2002e904d763
Chris Bartlett <bartlettc@gmail.com>
2019-04-23 09:20:02 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lilli
+
+Simple program example based on [u/x32byTe](https://www.reddit.com/user/x32byTe)'s program [Yeet!](https://www.reddit.com/r/ProgrammerHumor/comments/bgdxwn/yeet/).  
---
+++ lilli.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#define llllli int
+#define llllil main
+#define llllii (
+#define lllill )
+#define lllili std
+#define llliii ::
+#define llilll cout
+#define llilli <<
+#define llilil "lilli"
+#define llilii ;
+#define lliill return
+#define lliili 0
+#define lliiil {
+#define lliiii }
+
+llllli llllil llllii lllill lliiil
+  lllili llliii llilll llilli llilil llilii
+  lliill lliili llilii
+lliiii
